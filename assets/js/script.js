d1b61be57e6dda4d905d8a26ef49ef33340209f5
Juan Diego Teheran Olmos <jdteheran@uninorte.edu.co>
2022-04-20 18:25:16 -0500

    segundo commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,32 @@ let input_cedula = document.getElementById('cedula')
 
 let btn_enviar = document.getElementById('enviar')
 
-input_cedula.addEventListener('keyup', (e) => {
-    let cedula = input_cedula.value
+let input_celular = document.getElementById('celular')
 
-    if (!(/^[1-9][0-9]*$/i.test(cedula))) {
-        input_cedula.value = cedula.replaceAll(e.key, '')
-    }
+input_celular.addEventListener('keypress', (e) => {
+    e.preventDefault()
+
+    validacion_numerica(input_celular, e.key)
 })
+
+input_cedula.addEventListener('keypress', (e) => {
+    e.preventDefault()
+
+    validacion_numerica(input_cedula, e.key)
+})
+
+function validacion_numerica(input, letra) {
+    if ((/[0-9]/i.test(letra))) {
+        input.value = input.value + letra
+    }
+}
 
 btn_enviar.addEventListener('click', () => {
     let inputs = document.querySelectorAll('input')
 
     //inputs.remove(btn_enviar)
+
+    let empleado = {}
 
     inputs.forEach(input => {
 
@@ -24,7 +38,37 @@ btn_enviar.addEventListener('click', () => {
             alert(`Campo ${input.getAttribute('placeholder')} se encuentra vacío`)
         }
 
+        empleado = {
+            ...empleado,
+            [input.name]: input.value
+        }
+
     })
+
+    empleado = {
+        id: uuidv4(),
+        ...empleado
+    }
+
+    let template_empleado = `
+        <tr>
+            <td>${empleado.id}</td> 
+            <td>${empleado.apellido}</td> 
+            <td>${empleado.nombre}</td>
+            <td>${empleado.correo}</td> 
+            <td>
+                <button class="mostrar">Mostrar</button>
+                <button class="editar">Editar</button>
+                <button class="borrar">Borrar</button>
+            </td>
+        </tr>
+    `
+
+    let tbody = document.getElementById('cuerpo_tabla')
+
+    tbody.innerHTML = tbody.innerHTML + template_empleado
+
+    console.log(empleado);
 })
 
 function validacion(event) {
@@ -38,3 +82,34 @@ function validacion(event) {
     return true
 }
 
+let form_agregar = document.getElementById('form_agregar')
+
+form_agregar.style.display = 'none'
+
+let btn_agregar = document.getElementById('agregar')
+
+btn_agregar.addEventListener('click', () => {
+    form_agregar.style.display = 'block'
+    btn_agregar.style.display = 'none'
+})
+
+let btn_cancelar = document.getElementById('cancelar_info')
+
+btn_cancelar.addEventListener('click', () => {
+    form_agregar.style.display = 'none'
+    btn_agregar.style.display = 'block'
+})
+
+let btns_borrar = document.querySelectorAll('.borrar')
+
+btns_borrar.forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log(btn.parentElement.parentElement.remove());
+    })
+});
+
+function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
